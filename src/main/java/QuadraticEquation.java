bf5c1844a52df1005130c445ad1c62d473d7c060
Denis Stepulenok <super.denis@gmail.com>
2015-05-23 05:23:13 +0300

    Стандартное квадратное уравнение с одним решением

--- src/main/java/QuadraticEquation.java
+++ src/main/java/QuadraticEquation.java
@@ -3,4 +3,15 @@
  */
 public class QuadraticEquation {
 
+    /**
+     * Решение квадратного уравнения: ax^2 + bx + c = 0
+     *
+     * @param a коэффициент при x^2
+     * @param b коэффициент при x
+     * @param c константа
+     * @return корни уравнения (значения x)
+     */
+    public static double[] solve(double a, double b, double c) {
+        return new double[]{-b / (2 * a)};
+    }
 }
--- src/test/java/QuadraticEquationTest.java
+++ src/test/java/QuadraticEquationTest.java
@@ -1,9 +1,19 @@
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Тесты для решения квадратного уравнения
  */
 public class QuadraticEquationTest extends Assert {
+    public static final double EPS = 1e-10;
 
-
+    /**
+     * Стандартное квадратное уравнение с одним решением
+     */
+    @Test
+    public void testSimpleOneRoot() {
+        assertArrayEquals("x^2 = 0", new double[]{0}, QuadraticEquation.solve(1, 0, 0), EPS);
+        assertArrayEquals("(x-1)^2 = x^2-2x+1", new double[]{1}, QuadraticEquation.solve(1, -2, 1), EPS);
+        assertArrayEquals("2(x-1)^2 = 2x^2-4x+2", new double[]{1}, QuadraticEquation.solve(2, -4, 2), EPS);
+    }
 }
